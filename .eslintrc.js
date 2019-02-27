63786141a2366cee7d17f08d948369429fa6849f
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-02-27 11:55:13 +0900

    $ npm i -D babel-eslint eslint eslint-config-prettier eslint-loader eslint-plugin-prettier eslint-plugin-vue prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'semi': [2, 'always'],
+    'prettier/prettier': [
+      'error',
+    ],
+  },
+};
