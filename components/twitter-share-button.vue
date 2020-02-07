df907c469268eaeaae33ecc0ddbd5c2193deb375
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2020-02-08 00:25:05 +0900

    Fix tweet button

--- components/twitter-share-button.vue
+++ components/twitter-share-button.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 <span>
-  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+  <a href="https://twitter.com/intent/tweet" class="twitter-share-button">Tweet</a>
   <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </span>
 </template>
